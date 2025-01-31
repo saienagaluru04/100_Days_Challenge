8fe1fbd9bfad451e5ef4cdd1462361d11d6532d6
Sai Kumar Enagaluru <saienagaluru04@gmail.com>
2025-01-31 23:04:03 +0530

    day 1 code in python

---
+++ Python/day_1.py
@@ -0,0 +1,6 @@
+# Band Name Generator :-
+# ----------------------
+print("Welcome to the Band Name Generator.")
+city = input("What is your city that you are grew up in?\n")
+nick_name_of_you = input("What is your nick name?\n")
+print("Your band name could be:" + " " + city + " " + nick_name_of_you)
